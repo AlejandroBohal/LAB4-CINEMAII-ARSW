5b0cfebfe3e9fb4fec180d37e6ad0d72666deafc
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2020-09-06 19:37:56 -0500

    Parte 1 terminada

--- src/main/java/edu/eci/arsw/cinema/controllers/CinemaAPIController.java
+++ src/main/java/edu/eci/arsw/cinema/controllers/CinemaAPIController.java
@@ -5,9 +5,19 @@
  */
 package edu.eci.arsw.cinema.controllers;
 
-import java.util.LinkedHashSet;
+
+import java.util.List;
 import java.util.Set;
+
+import edu.eci.arsw.cinema.model.Cinema;
+import edu.eci.arsw.cinema.model.CinemaFunction;
+import edu.eci.arsw.cinema.persistence.CinemaException;
+import edu.eci.arsw.cinema.persistence.CinemaPersistenceException;
+import edu.eci.arsw.cinema.services.CinemaServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +25,44 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author cristian
  */
+@RestController("/")
 public class CinemaAPIController {
-    
+    @Autowired
+    private CinemaServices cinemaServices;
+    @RequestMapping(value = "/cinemas",method = RequestMethod.GET)
+    public ResponseEntity<?> controllerGetResourceCinema() throws ResourceNotFoundException {
+        try {
+            Set<Cinema> data = cinemaServices.getAllCinemas();
+            return new ResponseEntity<>(data, HttpStatus.ACCEPTED);
+        } catch (CinemaPersistenceException e) {
+            throw  new ResourceNotFoundException(e.getMessage(),e);
+        }
+    }
+    @RequestMapping( value = "/cinemas/{name}",method = RequestMethod.GET)
+    public ResponseEntity<?> getCinemaByName(@PathVariable String name) throws ResourceNotFoundException {
+        try {
+            Cinema data = cinemaServices.getCinemaByName(name);
+            return new ResponseEntity<>(data, HttpStatus.ACCEPTED);
+        } catch (CinemaException | CinemaPersistenceException e) {
+            throw  new ResourceNotFoundException(e.getMessage(),e);
+        }
+    }
+    @RequestMapping( value = "/cinemas/{name}/{date}",method = RequestMethod.GET)
+    public ResponseEntity<?> getFunctionsByNameAndDate(@PathVariable String name,@PathVariable String date) throws ResourceNotFoundException {
+        try {
+            List<CinemaFunction> data = cinemaServices.getFunctionsByCinemaAndDate(name,date);
+            return new ResponseEntity<>(data, HttpStatus.ACCEPTED);
+        } catch (CinemaPersistenceException e) {
+            throw  new ResourceNotFoundException(e.getMessage(),e);
+        }
+    }
+    @RequestMapping( value = "/cinemas/{name}/{date}/{moviename}",method = RequestMethod.GET)
+    public ResponseEntity<?> getFunctionsByNameAndDateAndMovie(@PathVariable String name,@PathVariable String date,@PathVariable String moviename) throws ResourceNotFoundException {
+        try {
+            CinemaFunction data = cinemaServices.getFunctionByCinemaAndDateAndMovie(name,date,moviename);
+            return new ResponseEntity<>(data, HttpStatus.ACCEPTED);
+        } catch (CinemaPersistenceException e) {
+            throw  new ResourceNotFoundException(e.getMessage(),e);
+        }
+    }
 }
